3a8711861b889ba32d5915f9e7304bddca4bfd0b
FairozAhmadSheikh <fairozahmadsheikh@gmail.com>
2026-09-01 12:54:33 +0530

    setting up secrets algoithm token_expiry , fake database etc

---
+++ social_media_api/Oauth2_JWT_HASHING.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI,HTTPException,Depends
+from jose import jwt,JWTError
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+from datetime import datetime,timedelta,timezone
+from passlib.context import CryptContext
+
+
+app=FastAPI()
+
+
+SECRET_KEY="Mysecretkey"
+ALGORITHM="HS256"
+ACCESS_TOKEN_EXPIRY_MINUTES=30
+
+
+pwd_context=CryptContext(schemes=['bcrypt'])
+oauth2_schema=OAuth2PasswordBearer(tokenUrl="login")
+
+
+fake_database_user={
+    "admin":{
+        "username":"admin",
+        "password":pwd_context.hash("1234")
+    }
+}
+
+
+
+
+@app.post("/login")
+def login():
+    return{}
